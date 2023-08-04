23c160531e1345c170f292a26c908d2c04383832
Worlako Dzokoto <worlaco.dzo@gmail.com>
2023-08-04 10:34:08 +0000

    Added public subnet

--- vpc/main.tf
+++ vpc/main.tf
@@ -18,27 +18,27 @@ resource "aws_internet_gateway" "main" {
   })
 }
 
-resource "aws_route_table" "main" {
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   tags = merge(local.tags, {
-    "Name" = var.route_table_name
+    "Name" = format("public-%s", var.route_table_name)
   })
 }
 
-resource "aws_route_table" "main" {
+resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
   tags = merge(local.tags, {
-    "Name" = var.route_table_name
+    "Name" = format("private-%s", var.route_table_name)
   })
 }
 
 
 resource "aws_route" "public" {
-  route_table_id         = aws_route_table.main
+  route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.main
+  gateway_id             = aws_internet_gateway.main.id
 
 
   timeouts {
@@ -69,6 +69,15 @@ resource "aws_subnet" "private" {
   availability_zone = element(local.azones, index(var.private_subnets, each.key))
 
   tags = merge(local.tags, {
-    "Name" = format("%s-public-%s", var.subnet_name, index(var.private_subnets, each.key))
+    "Name" = format("%s-private-%s", var.subnet_name, index(var.private_subnets, each.key))
   })
+}
+
+
+resource "aws_route_table_association" "public" {
+    for_each = aws_subnet.public
+    subnet_id = each.value.id
+    route_table_id = aws_route_table.main.id
+    
+  
 }
