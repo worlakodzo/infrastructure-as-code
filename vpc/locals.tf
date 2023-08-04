c7df963bf29a47230c819026643f7b405a4f4b01
Worlako Dzokoto <worlaco.dzo@gmail.com>
2023-08-04 12:27:17 +0000

    Added route table  association

--- vpc/locals.tf
+++ vpc/locals.tf
@@ -16,7 +16,7 @@ locals {
   public_subnet_ids = [for key, subnet in aws_subnet.public : subnet.id]
 
   # GET ALL PRIVATE ROUTE TABLE IDS
-  private_route_table_ids = [for key, route_table in aws_route_table.private: route_table.id]
+  private_route_table_ids = [for key, route_table in aws_route_table.private : route_table.id]
 
 }
 
