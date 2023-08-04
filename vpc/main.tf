72d29a94857fd83c715a612cb9d119baabb1b6cf
Worlako Dzokoto <worlaco.dzo@gmail.com>
2023-08-04 10:34:42 +0000

    Added public subnet

--- vpc/main.tf
+++ vpc/main.tf
@@ -75,9 +75,9 @@ resource "aws_subnet" "private" {
 
 
 resource "aws_route_table_association" "public" {
-    for_each = aws_subnet.public
-    subnet_id = each.value.id
-    route_table_id = aws_route_table.main.id
-    
-  
+  for_each       = aws_subnet.public
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.main.id
+
+
 }
